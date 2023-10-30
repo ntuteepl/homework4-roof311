510d9dfd12fe4d2d2a8d31e8d7482440beae1018
roof311 <146065407+roof311@users.noreply.github.com>
2023-10-30 20:25:21 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,a=0;
+    scanf_s("%d",&n);
+    int N[n];
+    for(int i=0;i<n;i++)
+    {
+    scanf_s("%d",&N[i]);
+
+    for(int x=2;x<=N[i];x++)
+    {
+        if(x==N[i])
+            {
+            a=1;
+            break;
+            }
+        if((N[i]%x)==0)
+        {
+          a=0;
+          break;
+        }
+    }
+    if(a==1) printf("YES ");
+    else printf("NO ");
+
+    }
+
+        }
+
 
